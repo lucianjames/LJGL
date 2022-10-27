b99371c5e32f7b48cf00d96c88412fd053a9fa2f
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-27 16:06:25 +0100

    Update initHelper.h

--- src/initHelper.h
+++ src/initHelper.h
@@ -13,7 +13,7 @@ void basic_framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
 
-void init(int glfw_version_major, int glfw_version_minor, bool use_compat=false){
+GLFWwindow* init(int w, int h, const char* title, int glfw_version_major, int glfw_version_minor, bool use_compat=false){
     if(!glfwInit()){ 
         std::cout << "Error: Could not initialize GLFW" << std::endl; 
         return; 
@@ -21,6 +21,14 @@ void init(int glfw_version_major, int glfw_version_minor, bool use_compat=false)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glfw_version_major);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glfw_version_minor);
     glfwWindowHint(GLFW_OPENGL_PROFILE, (use_compat)? GLFW_OPENGL_COMPAT_PROFILE : GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(w, h, title, NULL, NULL);
+    if(window == NULL){
+        std::cout << "Error: Could not create window" << std::endl;
+        glfwTerminate();
+        return NULL;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, basic_framebuffer_size_callback);
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to initialize GLAD" << std::endl;
         return;
@@ -28,18 +36,6 @@ void init(int glfw_version_major, int glfw_version_minor, bool use_compat=false)
     glEnable(GL_DEPTH_TEST); // Enable depth testing
     glEnable(GL_CULL_FACE); // Cull back faces
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f); // Set the background color to a dark grey.
-}
-
-GLFWwindow* createGlfwWindow(int width, int height, const char* title){
-    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
-    if(window == NULL){
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return NULL;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, basic_framebuffer_size_callback);
-    return window;
 }
 
 }
