6f42db9eb00bcacda291f87b237ed6753d0ab4f4
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-10-27 20:04:09 +0100

    Delete it all, going to remake

--- src/world.h
+++ src/world.h
@@ -6,37 +6,18 @@
 namespace LJGL{
 
 class world{
-protected:
+public:
     camera* cam;
     GLFWwindow* window;
-
-public:
-    std::vector<model> models;
-    std::vector<model_EBO> models_EBO; // This is hacky, need to make the model class more flexible.
+    model_EBO* model;
 
     world(GLFWwindow* window){
         this->cam = new camera(window);
         this->window = window;
     }
-    
-    void render(){
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color and depth buffers
-        this->cam->processInput();
-        for(auto &m : models_EBO){
-            m.m_projection = this->cam->getPerspectiveMatrix();
-            m.m_view = this->cam->getViewMatrix();
-            m.draw();
-        }
-        glfwSwapBuffers(this->window);
-        glfwPollEvents();
-    }
-
-    unsigned int addLoadModel_EBO(std::string path){
-        model_EBO m;
-        this->models_EBO.push_back(m);
-        this->models_EBO.back().readVBO(path + ".vbo");
-        this->models_EBO.back().readEBO(path + ".ebo");
-        return this->models_EBO.size() - 1;
+    ~world(){
+        delete cam;
+        delete model;
     }
 
 };
