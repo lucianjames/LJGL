a3b943d55df56cf9b8a959598c35e106d48bc775
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-11-08 19:39:17 +0000

    Update texture.h

--- src/texture.h
+++ src/texture.h
@@ -20,14 +20,14 @@ public:
         // set the texture wrapping/filtering options (on the currently bound texture object):
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         // load and generate the texture:
         stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
         unsigned char* data = stbi_load(path, &this->width, &this->height, &this->nrChannels, 0);
         if (data){
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this->height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-            glGenerateMipmap(GL_TEXTURE_2D);
+            //glGenerateMipmap(GL_TEXTURE_2D);
         }
         else{
             std::cout << "ERR: Failed to load texture " << path << std::endl;
