571b16cb26b7f07f2e7072a57a3aadfd08e8e0e6
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-11-08 19:41:18 +0000

    Update texture.h

--- src/texture.h
+++ src/texture.h
@@ -23,7 +23,7 @@ public:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         // load and generate the texture:
-        stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+        //stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
         unsigned char* data = stbi_load(path, &this->width, &this->height, &this->nrChannels, 0);
         if (data){
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this->height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
